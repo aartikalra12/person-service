243185b324b2fb1539c66e11d32ea3b8af1a39ac
aartikalra12 <145558471+aartikalra12@users.noreply.github.com>
2025-05-02 13:01:31 +0200

    Create PersonStack.java

---
+++ src/main/com/tikkie/person/PersonStack.java
@@ -0,0 +1,77 @@
+// src/main/java/com/tikkie/person/PersonStack.java
+package com.tikkie.person;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.apigateway.Resource;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Code;
+import software.constructs.Construct;
+
+public class PersonStack extends Stack {
+    public PersonStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public PersonStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // DynamoDB Table
+        Table personTable = Table.Builder.create(this, "PersonsTable")
+            .partitionKey(Attribute.builder().name("id").type(AttributeType.STRING).build())
+            .tableName("Persons")
+            .removalPolicy(software.amazon.awscdk.RemovalPolicy.DESTROY)
+            .build();
+
+        // Create Person Lambda
+        Function createPersonFunction = Function.Builder.create(this, "CreatePersonFunction")
+            .runtime(Runtime.JAVA_17)
+            .handler("com.tikkie.person.handler.CreatePersonHandler::handleRequest")
+            .code(Code.fromAsset("target/person-service-1.0-SNAPSHOT.jar"))
+            .environment(java.util.Map.of("TABLE_NAME", personTable.getTableName()))
+            .build();
+
+        // Get Persons Lambda
+        Function getPersonsFunction = Function.Builder.create(this, "GetPersonsFunction")
+            .runtime(Runtime.JAVA_17)
+            .handler("com.tikkie.person.handler.GetPersonsHandler::handleRequest")
+            .code(Code.fromAsset("target/person-service-1.0-SNAPSHOT.jar"))
+            .environment(java.util.Map.of("TABLE_NAME", personTable.getTableName()))
+            .build();
+
+        // Health Check Lambda
+        Function healthCheckFunction = Function.Builder.create(this, "HealthCheckFunction")
+            .runtime(Runtime.JAVA_17)
+            .handler("com.tikkie.person.handler.HealthCheckHandler::handleRequest")
+            .code(Code.fromAsset("target/person-service-1.0-SNAPSHOT.jar"))
+            .build();
+
+        // Grant permissions to Lambdas
+        personTable.grantReadWriteData(createPersonFunction);
+        personTable.grantReadData(getPersonsFunction);
+
+        // API Gateway
+        LambdaRestApi api = LambdaRestApi.Builder.create(this, "PersonServiceApi")
+            .handler(healthCheckFunction) // default handler
+            .proxy(false)
+            .build();
+
+        Resource personResource = api.getRoot().addResource("person");
+        personResource.addMethod("POST", new software.amazon.awscdk.services.apigateway.LambdaIntegration(createPersonFunction));
+        personResource.addMethod("GET", new software.amazon.awscdk.services.apigateway.LambdaIntegration(getPersonsFunction));
+
+        api.getRoot().addResource("health").addMethod("GET", new software.amazon.awscdk.services.apigateway.LambdaIntegration(healthCheckFunction));
+    }
+
+    public static void main(final String[] args) {
+        App app = new App();
+        new PersonStack(app, "PersonServiceStack");
+        app.synth();
+    }
+}
