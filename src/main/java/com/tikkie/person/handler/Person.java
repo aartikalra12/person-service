e6c07ffcf054b200d6cfb92cd188fd77f337258c
aartikalra12 <145558471+aartikalra12@users.noreply.github.com>
2025-05-11 11:09:27 +0200

    Update Person.java

--- src/main/java/com/tikkie/person/handler/Person.java
+++ src/main/java/com/tikkie/person/handler/Person.java
@@ -9,6 +9,46 @@ public class Person {
 
     // Getters and setters
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public Address getAddress() {
+        return this.address;
+    }
+
     public static class Address {
         private String street;
         private String postalCode;
@@ -16,6 +56,39 @@ public class Person {
         private String country;
 
         // Getters and setters
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public void setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getStreet(){
+            return this.street;
+        }
+
+        public String getPostalCode(){
+            return this.postalCode;
+        }
+
+        public String getCity(){
+            return this.city;
+        }
+
+        public String getCountry(){
+            return this.country;
+        }
+        
     }
 
     // Getters and setters
