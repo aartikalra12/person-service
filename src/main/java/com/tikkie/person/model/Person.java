3f0bf6fec17e52846360a15a95f070a1290c908e
aartikalra12 <145558471+aartikalra12@users.noreply.github.com>
2025-05-11 23:43:33 +0200

    Update Person.java

--- src/main/java/com/tikkie/person/model/Person.java
+++ src/main/java/com/tikkie/person/model/Person.java
@@ -1,17 +1,9 @@
 package com.tikkie.person.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
 public class Person {
     private String id;
-    
-    @NotBlank(message = "First name is required")
     private String firstName;
-    
-    @NotBlank(message = "Last name is required")
     private String lastName;
-    
     private String phone;
     private Address address;
 
